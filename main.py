3aa038cf21fbe74fd33d7f5c2e0e57775e43b3a0
esrakubra <esra96elden@gmail.com>
2022-12-10 02:41:52 +0300

    oxox

---
+++ main.py
@@ -0,0 +1,21 @@
+nub1= int(input('Birinci Sayıyı Giriniz:'))
+nub2= int(input('İkinci Sayıyı Giriniz:'))
+
+op = input('Bir İşlem Seçiniz(+, -, *, /):')
+
+if op == '+':
+    print(nub1, '+', nub2, '=', nub1 + nub2)
+elif op == '-':
+    print(nub1,'+', nub2, '=', nub1 + nub2)
+elif op == '*':
+    print(nub1,'+', nub2, '=', nub1 + nub2)
+elif op == '/':
+    print(nub1, '+', nub2, '=', nub1 + nub2)
+else:
+    print('Programımızda Böyle Bir İşlem Geçerli Değildi')
+
+
+
+    
+    
+    
